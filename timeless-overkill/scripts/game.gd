85ad72bdd53bbc17ecb8cf6e8f12162def875321
DevGreem <ian.pichardo@hotmail.com>
2026-07-26 01:03:11 -0400

    fix: Fixed score counter saving

--- timeless-overkill/scripts/game.gd
+++ timeless-overkill/scripts/game.gd
@@ -2,5 +2,8 @@ extends Node2D
 
 @onready var kill_screen = $CanvasLayer/kill_screen
 
+func _ready() -> void:
+	ScoreCounter.reset_score()
+
 func _on_player_kill_screen_show() -> void:
 	kill_screen.show()
